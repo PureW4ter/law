fc78abbefe3ff3e71e5def96a7ff774ef5a57a5e
崔航 <cuihang@cuihangdeMacBook-Pro.local>
2017-03-17 13:59:56 +0800

    完成链接

--- web/main/webapp/js/lawyer_reply.js
+++ web/main/webapp/js/lawyer_reply.js
@@ -1,8 +1,21 @@
-define(['ajaxhelper', 'utility', 'component/time_button'], function(ajaxHelper, util, timeBtn) {
-    var Regist = {
-        initialize: function() {
-           timeBtn.initialize("i_getcode_btn");
-        },
+define(['component/header','ajaxhelper', 'utility'], function(header, ajaxHelper, util) {
+    var LawyerReply = {
+        initialize :function(){
+			//request
+			this._sendRequest();
+		},
+		_sendRequest :function(){
+			this._render();
+		},
+		_render:function(){
+			this._registEvent();
+		},
+		_registEvent:function(){
+			$("#i_view").off("click", this._view).on("click", {ctx: this}, this._view);
+		},
+		_view:function(e){
+			window.location = "view_reply.html";
+		},
     };
-    return Regist;
+    return LawyerReply;
 });
