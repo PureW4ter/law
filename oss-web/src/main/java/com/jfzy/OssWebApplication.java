609b876d95b339d81524aa682c8996aaba465632
hualei <hualei@192-169-24-146.dsl.beggstelco.net>
2017-06-01 11:10:30 +0800

    fix bug

--- oss-web/src/main/java/com/jfzy/OssWebApplication.java
+++ oss-web/src/main/java/com/jfzy/OssWebApplication.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
