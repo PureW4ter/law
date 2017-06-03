65264ec3f1eeb2c15019cbdb793b60a6ecca9d22
hualei <hualei@192.168.100.111>
2017-06-03 12:50:43 +0800

    aaa

--- jf-service/src/main/java/com/jfzf/core/Constants.java
+++ jf-service/src/main/java/com/jfzf/core/Constants.java
@@ -4,11 +4,13 @@ public class Constants {
 	public static final String SECRET = "bb5ffbb33361961a2e2fb078ebd5ce31";
 	public static final String APP_ID = "wx3eb0212d543f4752";
 	public static final String WX_ID = "jianfa-lawyer";
+	public static final String PAY_APP_ID = "";
+	public static final String PAY_SECRET = "QLQ9o8BQyaILKJqJDI5SqBFM78uz740";
 	public static final String MCH_ID = "1430959102";
 	public static final String DEVICE_INFO = "WEB";
 	public static final String SIGN_TYPE = "MD5";
 	public static final String FEE_TYPE = "CNY";
-	public static final String TRADE_TYPE="JSAPI";
+	public static final String TRADE_TYPE = "JSAPI";
 
 	public static String PRODUCT_CODE_ZIXUN = "Y";
 	public static String PRODUCT_CODE_ZIXUNP = "YP";
