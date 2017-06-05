e76e3872bad72e438f39ee6ffeb475b977de3dc3
hualei <hualei@192.168.1.163>
2017-06-05 22:47:19 +0800

    change role

--- jf-service/src/sql/init.sql
+++ jf-service/src/sql/init.sql
@@ -13,9 +13,9 @@ VALUES
 	
 INSERT INTO `jf_oss_role` (`id`, `name`, `desc`, `status`)
 VALUES
-	(1,'super_admin','特别管理员',0),
-	(2,'admin','一般管理员',0),
-	(3,'order_admin','订单管理员',0);
+	(1,'admin','高级管理员',0),
+	(2,'editor','简法编辑部',0),
+	(3,'csr','订单管理员',0);
 	
 
 INSERT INTO `jf_property` (`id`, `type_id`, `name`)
