ae4decb32e8d10c2a6a2f563a15b9d59cf6cadc3
崔航 <56367582@qq.com>
2017-06-16 23:25:46 +0800

    静态页面提交

---
+++ static/bk/js/diaocha_reply.js
@@ -0,0 +1,66 @@
+"use strict";
+
+define(['component/nav_bar','component/header', 'ajaxhelper', 'utility'],function(nav_bar, header, ajaxHelper, util){
+	var DiaochaReply = {
+		id: "",
+		initialize :function(){
+			nav_bar.initialize("i_navbar", 7);
+			header.initialize("i_header", "调查回复");
+			//body
+			this.mainBox = $('#i_mainbox');
+			this.tplfun = _.template($("#i_tpl").html());
+			//request
+			this._sendRequest();
+		},
+		_sendRequest :function(){
+			var params = {
+				"id": util.getQueryParameter("id"),
+			};
+			ajaxHelper.get("http://" + window.frontJSHost + "/api/order/getreply",
+                params, this, this._render);
+		},
+		_render:function(data){
+			if(data.r.lawyerReplyVo){
+                this.id = data.r.lawyerReplyVo.id;
+            }
+			this.mainBox.html(this.tplfun({"result": data}));
+			this._xheditor();
+			this._registEvent();
+		},
+		_registEvent: function () {
+            $('#i_save').off("click", this._doSave).on("click", {"ctx":this}, this._doSave);
+            $('#i_cancel').off("click", this._doCancel).on("click", {"ctx":this}, this._doCancel);
+        },
+        _xheditor: function(){
+            var url="http://" + window.frontJSHost + "/api/pic/upload";
+            this.editorReply = $('#i_reply').xheditor({width:'100%',height:200, html5Upload:false, upImgUrl:url, upImgExt:"jpg,jpeg,gif,png"});
+        },
+        _doSave:function(e){
+            var params = {
+                orderId: util.getQueryParameter("id"),
+                simpleReply: e.data.ctx.editorReply?e.data.ctx.editorReply.getSource():"",
+                hasHukou: 0
+            }
+            if (e.data.ctx.id) { 
+                params.id=e.data.ctx.id;
+                ajaxHelper.post("http://" + window.frontJSHost + "/api/order/reply",
+                    params, e.data.ctx, function(){
+                        util.showToast("更新成功！", function(){
+                            window.location = "lawyer_home.html";
+                        })
+                    });
+            } else {
+                ajaxHelper.post("http://" + window.frontJSHost + "/api/order/reply",
+                    params, e.data.ctx, function(){
+                        util.showToast("保存成功！", function(){
+                            window.location = "lawyer_home.html";
+                        })
+                    });
+            }
+        },
+        _doCancel:function(e){
+            window.location = "lawyer_home.html";
+        }
+	};
+	return DiaochaReply;
+});
---
+++ static/bk/js/lawyer_home.js
@@ -0,0 +1,42 @@
+define(['component/nav_bar','component/header', 'ajaxhelper', 'utility'], function (nav_bar, header, ajaxHelper, util) {
+    var LawyerHome = {
+        currentPage: 0,
+        size: 30,
+        initialize: function () {
+            //nav_bar
+            nav_bar.initialize("i_navbar", 7);
+            header.initialize("i_header", "律师主页");
+            this.mainBox = $('#i_mainbox');
+            this.tplFun = _.template($("#i_tpl").html());
+            this._sendRequest();
+        },
+        _sendRequest: function () {
+            var userData= $.parseJSON(localStorage.userInfo);
+            var params = {
+                "lawyerId": userData.id,
+                "page": this.currentPage, 
+                "size": this.size
+            };
+            ajaxHelper.get("http://" + window.frontJSHost + "/api/order/lawyer",
+                params, this, this._render, null);
+        },
+        _render: function (data) {
+            this.mainBox.html(this.tplFun({'result': data}));
+            this._registEvent();
+        },
+        _registEvent: function () {
+            $('.j_reply').off("click", this._goReply).on("click", this._goReply);
+        },
+        _goReply:function(e){
+            var oid = $(e.target).parents("tr").data("id");
+            var productId = $(e.target).parents("tr").data("pid");
+            var productCode = $(e.target).parents("tr").data("pcode");
+            if(productCode == util.PRODUCT_CODE_HUKOU || productCode == util.PRODUCT_CODE_CHAFENG || productCode == util.PRODUCT_CODE_HUKOU_XQ){
+                window.location = "diaocha_reply.html?id=" + oid;
+            }else{
+                window.location = "zixun_reply.html?id=" + oid;
+            }
+        }
+    };
+    return LawyerHome;
+});
