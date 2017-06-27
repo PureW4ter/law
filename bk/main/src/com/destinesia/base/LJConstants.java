bb8a0421bf04f79668891cee83c91673cfe55c1b
崔航 <56367582@qq.com>
2017-06-27 16:04:19 +0800

    静态页更新

--- bk/main/src/com/destinesia/base/LJConstants.java
+++
@@ -1,8 +0,0 @@
-package com.destinesia.base;
-
-public class LJConstants {
-	public static String apiBaseUrl="http://gw.api.taobao.com/router/rest";
-	public static String appkey="LTAITP8hZ7khyeWE";
-	public static String secret="VRzzwLtpfrCDWR2ZHgAxl2IzSwHZSc";
-	public static int pageSize = 30;
-}
